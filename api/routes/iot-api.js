359aa2367e2f893ee32ab078416e36ea4565e7f1
lutfi-haslab <108409669+lutfi-haslab@users.noreply.github.com>
2022-12-15 14:03:22 +0700

    update offset last 2500

--- api/routes/iot-api.js
+++ api/routes/iot-api.js
@@ -3,7 +3,7 @@ import { excuteQuery } from "../db.js";
 async function RoutesIoT(route, options) {
   route.get("/api/iot/ptkp", async (req, res) => {
     try {
-      let data = await excuteQuery("SELECT * FROM ptkp_sensor ORDER BY id DESC", []);
+      let data = await excuteQuery("SELECT * FROM ptkp_sensor ORDER BY id DESC LIMIT 2500", []);
       return res.code(200).send(data);
     } catch (err) {
       return res.code(500).send(err);
